5684aaa80efe059080d092406cf4c41c57d70950
hoangminhkhoi <khoihoang252007@gmail.com>
2026-09-13 00:06:54 +0700

    error handle

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const app = express()
 
 const fieldRouter = require('./routes/PitchRouter')
 const timeSlotRouter = require('./routes/TimeSlotRouter')
+const globaErrorlHandler = require('./controller/ErrorController')
+const AppError = require('./utils')
 
 
 app.use(express.json())
@@ -13,5 +15,10 @@ app.use(morgan('dev'))
 app.use('/api/pitch', fieldRouter)
 app.use('/api/timeSlot', timeSlotRouter)
 
+app.all('*', (err, req, res, next) => {
+    next(new AppError(`Can not find ${req.originalUrl} in this server`, 404))
+})
+
+app.use(globalHandler)
 
 module.exports = app
---
+++ src/controller/ErrorController.js
@@ -0,0 +1,30 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+const sendErrorProd = (err, res) => {
+    if (err.isOperational === true) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        })
+    } else {
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong'
+        })
+    }
+}
+
+module.exports = (err, req, res, next) => {
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res)
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res)
+    }
+}
